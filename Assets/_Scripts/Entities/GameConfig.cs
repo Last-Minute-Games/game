4c8a5a250217fcf4533c10a09707eb575f6a8950
sebvu <santosjester738@gmail.com>
2025-11-02 04:27:37 -0600

    Create global nether GameConfig file

--- Assets/_Scripts/Entities/GameConfig.cs
+++ Assets/_Scripts/Entities/GameConfig.cs
@@ -1,9 +1,12 @@
-
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Config/Game Config", fileName = "GameConfig")]
 public class GameConfig : ScriptableObject
 {
+    /// <summary>
+    /// This file holds all configuration options for The Nether.
+    /// If you want to tweak some global variable bullshit please add it here.
+    /// </summary>
     [Header("Player Defaults")]
     public int defaultBaseEnergy = 3;
     public int defaultMaxEnergy = 3;
