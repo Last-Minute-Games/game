d73375f2eb364822518fd2948912521ff1e81107
sebvu <santosjester738@gmail.com>
2025-11-13 02:04:10 -0600

    Add MusicManager.cs

---
+++ Assets/Scripts/Core/MusicManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("Tutorial Dream Settings")]
+    public AudioClip dreamIntro;
+    public AudioClip dreamLoop;
+
+    [Header("Default Settings")] 
+    public AudioClip defaultIntro;
+    public AudioClip defaultLoop;
+    
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        source.clip = dreamIntro;
+        source.loop = true;
+        source.playOnAwake = false;
+        source.volume = 0.5f;
+        
+        if (defaultLoop != null)
+        {
+            source.clip = defaultLoop;
+            source.loop = true;
+
+            if (defaultIntro)
+            {
+                source.clip = defaultIntro;
+                source.loop = false;
+                source.Play();
+                
+                StartCoroutine(WaitForDefaultIntro());
+            }
+            else
+            {
+                source.Play();
+            }
+        }
+    }
+    
+    private IEnumerator WaitForDefaultIntro()
+    {
+        // Wait while the audio source is playing
+        while (source.isPlaying)
+        {
+            yield return null; // Yield control back to Unity for one frame
+        }
+
+        // This code will execute once the audio has finished playing
+        Debug.Log("Audio has finished playing! Executing action...");
+        // Place your desired action here, e.g.,
+        source.clip = defaultLoop;
+        source.loop = true;
+        source.Play();
+    }
+    
+    public AudioSource GetAudioSource()
+    {
+        return source;
+    }
+    
+    public void SetAudioClip(AudioClip clip, bool loop = false)
+    {
+        source.clip = clip;
+        source.loop = loop;
+    }
+    
+    public void FadeAndPlay(float endValue, float duration)
+    {
+        source.Play();
+        source.DOFade(endValue, duration).SetEase(Ease.Linear);
+    }
+    
+    public void FadeAndStop(float endValue, float duration)
+    {
+        source.DOFade(endValue, duration).SetEase(Ease.Linear).OnComplete(() => source.Stop());
+    }
+    
+    public void Play()
+    {  
+        source.Play();
+    }
+
+    void Start()
+    {
+        
+    }
+}
