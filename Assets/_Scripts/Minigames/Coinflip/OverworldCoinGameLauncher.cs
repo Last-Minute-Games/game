ec3d4b68cbde4bae004a940ef78ed6c06fd10d3c
Aurelio Aguirre <aurelioaguirre21@gmail.com>
2025-12-03 12:59:32 -0600

    coin trigger is set to E and centered txt
    pleasework

--- Assets/_Scripts/Minigames/Coinflip/OverworldCoinGameLauncher.cs
+++ Assets/_Scripts/Minigames/Coinflip/OverworldCoinGameLauncher.cs
@@ -52,7 +52,7 @@ public class OverworldCoinGameLauncher : MonoBehaviour
         if (player == null) return;
         
         float distance = Vector3.Distance(transform.position, player.position);
-        if (distance <= interactDistance && Input.GetKeyDown(KeyCode.C))
+        if (distance <= interactDistance && Input.GetKeyDown(KeyCode.E))
             OpenCoinFlipPopup();
     }
 
