4e40faa03f0d54d81806a35ca8b5bda0158f2822
Jester <jester.sebastiansantos@gmail.com>
2026-03-21 23:08:00 -0500

    Fix card pile display and increase draw pile

--- Assets/Scripts/GameItems/Cards/Helpers/PileCountUI.cs
+++ Assets/Scripts/GameItems/Cards/Helpers/PileCountUI.cs
@@ -9,13 +9,28 @@ public class PileCountUI : MonoBehaviour
     public TMP_Text drawCountText;
     public TMP_Text discardCountText;
 
-    void Start()
+    private static PileCountUI _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
     {
         RefreshCounts();
     }
 
+    public static void RefreshNow()
+    {
+        _instance?.RefreshCounts();
+    }
+
     public void RefreshCounts()
     {
+        if (drawCountText == null || discardCountText == null)
+            return;
+
         if (playerManager == null || playerManager.cardManager == null)
         {
             drawCountText.text = "0";
