2fd3096066678c83ae6314eadb9a794a3727e074
Jester <jester.sebastiansantos@gmail.com>
2026-04-13 04:54:01 -0500

    Create CardView and new card prefab

---
+++ Assets/Scripts/OverhaulNetherScripts/Views/CardView.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+  [SerializeField] private TMP_Text title;
+
+  [SerializeField] private TMP_Text description;
+
+  [SerializeField] private TMP_Text mana;
+
+  [SerializeField] private SpriteRenderer iconSR;
+
+  [SerializeField] private SpriteRenderer iconBackground;
+
+  [SerializeField] private GameObject wrapper;
+}
